c1864e82708b70f79a9066b59f3f34393934c465
ChernyyOrel <chernyyorel.all@gmail.com>
2023-06-09 13:49:59 +0200

    Feat : movie card default (movies part)

--- views/movie/movielisting.php
+++ views/movie/movielisting.php
@@ -3,18 +3,20 @@
     ob_start();
 ?>
 
+
 <main class="movielist-main">
     <h2>All movies</h2>
 
     <div class="movielist-card-part">
         <?php
 
+        $lorem = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor reiciendis aspernatur unde mollitia pariatur ipsa qui eligendi consequatur facilis rem porro magnam officiis, eaque provident numquam modi dolores aliquid eos.";
         foreach ($films->fetchAll() as $movie) {
             echo 
             "<div class='movielist-card'>
-                <img class='movielist-banner' src='public/images/default-image.jpg' alt='".$movie["title"]." movie banner image'>
+                <img class='movielist-banner' src='public/images/default-image.png' alt='".$movie["title"]." movie banner image'>
                 <p class='movielist-card-title'>".$movie["title"]."</p>
-                <p class='movielist-card-description'>".$movie["summary"]."</p>
+                <p class='movielist-card-description'>".$lorem."</p>
             </div>";
         }
 
