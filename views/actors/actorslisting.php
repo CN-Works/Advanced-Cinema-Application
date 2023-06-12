b3aae4bceed21114505926242f84bdc927864790
ChernyyOrel <chernyyorel.all@gmail.com>
2023-06-12 11:02:56 +0200

    Feat : Actors list content with person's data

--- views/actors/actorslisting.php
+++ views/actors/actorslisting.php
@@ -3,14 +3,37 @@
     ob_start();
 ?>
 
-<p>test</p>
-<pre>
-<?php
-    foreach ($actors->fetchAll() as $actordata) {
-        echo $actordata["firstname"];
-    }
-?>
-</pre>
+<main class="actorpage-main">
+    <div class="actorpage-head">
+        <h2 class="actorpage-head-title">All actors</h2>
+        <img class="actorpage-head-image unselectable" src="public/images/actors.png" alt="Mask icon">
+    </div>
+    <?php
+        foreach ($actors->fetchAll() as $actordata) {
+            if ($actordata["sex"] == "m") {
+                $image = "public/images/man.png";
+                $pronoun = "He";
+            } else {
+                $image = "public/images/woman.png";
+                $pronoun = "She";
+            }
+
+            // Getting the birth date and date format
+            $actor_dob = date_create($actordata["dob"]);
+            $actor_dob = date_format($actor_dob,"l d F Y");
+
+            echo "
+            <div class='actorpage-card'>
+                <img class='actor-card-image' src=".$image." alt='person image'>
+                <div class='actorpage-card-texts'>
+                    <p>".$actordata["firstname"]." ".$actordata["lastname"]."</p>
+                    <p class='actorpage-card-dob'>".$pronoun." was born on ".$actor_dob."</p>
+                </div>
+            </div>
+            ";
+        }
+    ?>
+</main>
 
 
 
