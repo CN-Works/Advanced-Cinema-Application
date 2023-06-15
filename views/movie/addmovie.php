2035490622ec5ea6b428c1d8959a8b17853bb86f
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-06-15 11:39:55 +0200

    Feat : Genre selection

--- views/movie/addmovie.php
+++ views/movie/addmovie.php
@@ -5,7 +5,7 @@
 
 <main class="addmovie-main">
     <form class="addmovie-card" action="index.php?action=addingMovie" method="post">
-        <h2 class="addmovie-title" >Create a movie</h2>
+        <h2 class="addmovie-title" >Add a movie to the list</h2>
 
         <div class="addmovie-element">
             <h3 class="addmovie-element-title" >Title</h3>
@@ -13,13 +13,26 @@
         </div>
 
         <div class="addmovie-element">
-            <h3 class="addmovie-element-title">Production time</h3>
-            <input class="addmovie-element-input" type="text" name="prod_time" id="prod_time" placeholder="2000" required>
+            <h3 class="addmovie-element-title" >Summary</h3>
+            <input class="addmovie-element-input" type="text" id="summary" name="summary" value="" required>
         </div>
 
         <div class="addmovie-element">
-            <h3 class="addmovie-element-title">Production time</h3>
-            <input class="addmovie-element-input" type="text" name="prod_time" id="prod_time" placeholder="2000" required>
+            <h3 class="addmovie-element-title">Year of production</h3>
+            <input class="addmovie-element-input" type="date" name="prod_time" id="prod_time" placeholder="2000" required>
+        </div>
+
+        <div class="addmovie-element">
+            <h3 class="addmovie-element-title">Genre</h3>
+            <select class="addmovie-element-select" name="genre" id="genre" required>
+                <?php
+                    foreach ($availableGenres->fetchAll() as $genreData) {
+                        $genreId = $genreData['id'];
+                        $genreLabel = $genreData['genre_label'];
+                        echo "<option value='".$genreId."'>".$genreLabel."</option>";
+                    }
+                ?>
+            </select>
         </div>
 
         <button class="addmovie-button" type="submit">Submit</button>
